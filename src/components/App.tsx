04276740d06da25dfa528d195aa6c29a87aadd2f
Adrian Oviedo <adrian.oviedo@koerber-supplychain.com>
2024-08-08 14:38:58 -0400

    added css, routing and navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,19 +2,44 @@ import React from "react";
 import { User } from "../model/Model";
 import { AuthService } from "../services/AuthService";
 import { Login } from "./Login";
+import { Router, Route, Switch } from "react-router-dom";
+import History from "../utils/History";
+import { Navbar } from "./Navbar";
+import { Home } from "./Home";
+import { Profile } from "./Profile";
 
 interface AppState {
   user: User | undefined;
 }
 
 export class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {user: undefined};
+    this.serUser = this.serUser.bind(this);
+  }
+
   private authService: AuthService = new AuthService();
+
+  private serUser(user: User) {
+    this.setState({user: user});
+  }
 
   render(){
     return (
-      <div>
-        App works!!
-        <Login authService={this.authService}/>
+      <div className="wrapper">
+        <Router history={History}>
+          <div>
+            <Navbar user={this.state?.user}/>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/login">
+                <Login authService={this.authService} setUser={this.serUser}/>
+              </Route>
+              <Route exact path="/profile" component={Profile}/>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { AuthService } from "../services/AuthService";
+import { User } from "../model/Model";
 
 interface LoginProps {
     authService: AuthService;
+    setUser:(user: User) => void;
 }
 
 interface LoginState {
@@ -10,6 +12,10 @@ interface LoginState {
     password: string;
     loginAttempted: boolean;
     loginSucceded: boolean;
+}
+
+interface CustomEvent {
+    target: HTMLInputElement;
 }
 
 export class Login extends React.Component<LoginProps, LoginState> {
@@ -20,15 +26,48 @@ export class Login extends React.Component<LoginProps, LoginState> {
         loginSucceded: false
     };
 
+    private setUserName(event: CustomEvent) {
+        this.setState({userName: event.target.value});
+    }
+
+    private setPassword(event: CustomEvent) {
+        this.setState({password: event.target.value});
+    }
+
+    private async handleSubmit(event: SyntheticEvent) {
+        event.preventDefault();
+        this.setState({loginAttempted: true});
+        const result = await this.props.authService.login(
+            this.state.userName,
+            this.state.password
+        );
+        if (result) {
+            this.setState({loginSucceded: true});
+            this.props.setUser(result);
+        } else {
+            this.setState({loginSucceded: false});
+        }
+    }
+
     render(): React.ReactNode {
+        let loginMessage: any;
+        if (this.state.loginAttempted) {
+            if (this.state.loginSucceded) {
+                loginMessage = <label>Login succeded</label>;
+            } else {
+                loginMessage = <label>Login failed</label>;
+            }
+        }
+
         return (
             <div>
                 <h2>Please login</h2>
-                <form>
-                    <input value={this.state.userName}/><br/>
-                    <input value={this.state.password} type="password"/><br/>
+                <form onSubmit={e => this.handleSubmit(e)}>
+                    <input value={this.state.userName} onChange={e => this.setUserName(e)}/><br/>
+                    <input value={this.state.password} onChange={e => this.setPassword(e)} type="password"/><br/>
                     <input type="submit" value="Login"/>
                 </form>
+                {loginMessage}
             </div>
         );
     }
